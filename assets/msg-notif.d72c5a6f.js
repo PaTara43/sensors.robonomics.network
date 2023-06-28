5c861f25a96e493b40d661941686ea959b8c6915
PaTara43 <PaTara43@users.noreply.github.com>
2023-06-28 14:58:05 +0000

    deploy: 4b67c3811754c8870055dd072e1126ed8db9485d

--- assets/msg-notif.d72c5a6f.js
+++ assets/msg-notif.d72c5a6f.js
@@ -1,1 +1,1 @@
-const s="/assets/msg-notif.fbcb8f30.png";export{s as default};
+const s="/sensors.robonomics.network/assets/msg-notif.fbcb8f30.png";export{s as default};
--- assets/msg-recycle.eab79c00.js
+++ assets/msg-recycle.eab79c00.js
@@ -1,1 +1,1 @@
-const e="/assets/msg-recycle.2ff7eefc.png";export{e as default};
+const e="/sensors.robonomics.network/assets/msg-recycle.2ff7eefc.png";export{e as default};
